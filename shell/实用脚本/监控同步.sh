bd251abfed0e16af06e4030dc9003a955ad5dd77
九億年一夢 <150105727+Xstudyxue@users.noreply.github.com>
2024-05-22 13:55:02 +0800

    Update 监控同步.sh

--- shell/实用脚本/监控同步.sh
+++ shell/实用脚本/监控同步.sh
@@ -1,1 +1,39 @@
+#!/bin/bash
+#yum install epel-release -y
+#yum install  inotify-tools  -y
+
+DIRNAME='/data/app'
+inotifywait -mrq  -e 'create,close_write' --timefmt "%Y-%m-%d_%H:%M:%S.%s" --format "%T %w%f %e"  "${DIRNAME}" | while read line
+do
+source ${PWD}/intoify_config.conf
+##获取写入时间
+CREATE_TIME=$(echo ${line} | awk '{print $1}')
+##获取文件路径
+LOCAL_SOURCE=$(echo ${line} | awk '{print $2}')
+rsync -avz  ${DIRNAME}  ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}/ >${RSYNC_INFO_PATH} 2>>${RSYNC_ERR_PATH}
+###pushinfo
+##消息正文内容文件内容
+if [ "${OPEN_PUSH_INFO:-off}" == 'on' ];
+then
+cat >${MSG_TEXT_FILE}<<eof
+标题:文件同步消息通知
+服务器时间:${CREATE_TIME}
+同步来源服务器:${LOCAL_SERVER_IP}
+同步远程服务器:${REMOTE_HOST}
+同步远程目录:${REMOTE_PATH}
+日志详情:
+____________
+$(tail -2 ${RSYNC_INFO_PATH})
+eof
+##生成json消息体文件
+cat >${MSG_JSON_FILE}<<EOF
+{
+"msgtype": "text",
+"text": {
+"content": "$(cat ${MSG_TEXT_FILE})"}}
+EOF
+##消息推送
+curl -s  ${ROBOT} -H "${TYPE}" -d "$(cat ${MSG_JSON_FILE})" >/dev/null  2>${PUSH_ERR_LOG_PATH}
+fi
+done
 
